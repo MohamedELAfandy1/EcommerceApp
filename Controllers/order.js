f9c5de08695444ba5f160906fa699759102fcbb8
MohamedElAfndy1 <149269287+MohamedELAfandy1@users.noreply.github.com>
2024-08-05 19:22:28 +0300

    Disable Stripe

--- Controllers/order.js
+++ Controllers/order.js
@@ -1,4 +1,4 @@
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+// const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const asyncHandler = require("express-async-handler");
 const apiError = require("../Utils/apiError");
 const factory = require("./handlersFactory");
