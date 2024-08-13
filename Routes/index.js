f9a5a2accaec849b6de800e3c8c6c21d14f136d8
MohamedElAfndy1 <149269287+MohamedELAfandy1@users.noreply.github.com>
2024-08-14 00:38:34 +0300

    Update Route

--- Routes/index.js
+++ Routes/index.js
@@ -12,18 +12,17 @@ const cartRoute = require("./cart.js");
 const orderRoute = require("./order.js");
 
 const mountRoutes = (app) => {
-  app.use("/api/v1/category", categoryRoute);
-  app.use("/api/v1/subcategoies", subCategoryRoute);
-  app.use("/api/v1/brands", brandRoute);
-  app.use("/api/v1/products", productRoute);
-  app.use("/api/v1/users", usersRoute);
-  app.use("/api/v1/auth", authRoute);
-  app.use("/api/v1/reviews", reviewRoute);
-  app.use("/api/v1/wishlist", wishlistRoute);
-  app.use("/api/v1/address", addressRoute);
-  app.use("/api/v1/coupon", couponRoute);
-  app.use("/api/v1/cart", cartRoute);
-  app.use("/api/v1/order", orderRoute);
+  app.use("/category", categoryRoute);
+  app.use("/subcategoies", subCategoryRoute);
+  app.use("/brands", brandRoute);
+  app.use("/products", productRoute);
+  app.use("/users", usersRoute);
+  app.use("/auth", authRoute);
+  app.use("/reviews", reviewRoute);
+  app.use("/wishlist", wishlistRoute);
+  app.use("/address", addressRoute);
+  app.use("/coupon", couponRoute);
+  app.use("/cart", cartRoute);
+  app.use("/order", orderRoute);
 };
-
 module.exports = mountRoutes;
